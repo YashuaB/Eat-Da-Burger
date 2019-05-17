04374f288dfacb6e4d784149f7bbb9f0629a8b8c
yashua <yashuab.abraham@gmail.com>
2019-05-16 22:44:10 -0400

    fixed broken route

--- public/js/app.js
+++ public/js/app.js
@@ -1,6 +1,6 @@
 
 
-$(function() {
+$(document).ready(function() {
   $(".devour-burger").on("click", function(event) {
     var id = $(this).data("id")
     var newBurger = $(this).data("newBurger")
